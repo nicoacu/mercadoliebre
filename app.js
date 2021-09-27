73ebb54255ec35800d2d85b7815dd4fddfcfcf2e
Nicolas Acuna <nicoacu87@gmail.com>
2021-09-27 00:33:16 -0300

    Finished register mockup

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(3030, () => {
-    console.log('Server successfully started at http://localhost:3030');
+  console.log("Server successfully started at http://localhost:3030");
 });
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
 });
